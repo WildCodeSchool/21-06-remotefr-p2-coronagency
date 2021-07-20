ffa9fcb7b2c982c790b32274aaf1b93da144967b
RemiGuti <koulouffpro@gmail.com>
2021-07-20 17:17:58 +0200

    first navigation

---
+++ src/components/Info.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Info() {
+    return (
+        <div>
+            <h1>Je suis la page infos sur le coronavirus</h1>
+        </div>
+    )
+}
+
+export default Info
